11b43c54188eee3733a98cdc3c0560bea1b30196
cinmi <ikhwanelyas2@outlook.com>
2019-09-17 14:58:42 +0900

    Latihan Variabel

--- Latihan_2.py
+++ Latihan_2.py
@@ -1,5 +1,9 @@
 # Ini Praktik Git untuk MK Pemrograman Game
 # juga untuk latihan git push dan pull from terminal
 
+# Latihan Buat Variabel : 
+mata_kuliah = "Pemrograman Game"
 print (" Halo PyGame, ini Latihan 2...")
 print (" Halo Pemrograman Game, aku datang...")
+print (" Nama MK : %s " % mata_kuliah)
+
---
+++ f2.ls
@@ -0,0 +1,2 @@
+59a2cab4767428645a6dd2dafba05742  ./Latihan_2.py
+84f26c578a0edcf89ab0f76a9fa4a0fa  ./Latihan_1.py
